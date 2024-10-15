1b6836995e3d75cd574be52f670978f36ed71dcd
Chris Turner <23338096+christopherjturner@users.noreply.github.com>
2024-10-15 10:56:21 +0100

    first version of script

--- README.md
+++ README.md
@@ -1,2 +1,81 @@
 # aws-cli-minify
-A python script for minifying the aws-cli
+A python script for minifying the aws-cli.
+
+## Requirements
+- python3
+- `strip` command (from binutils, optional)
+- aws-cli zip file
+
+## Usage
+
+1. Download the AWS CLI V2.
+[https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html#cliv2-linux-install](https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html#cliv2-linux-install)
+
+2. Unzip the AWS CLI.
+
+```
+unzip awscliv2.zip
+```
+
+3. Create a minify.keep file and add services you *dont* want removed
+
+The file should be a plain text file with each service name on a new line.
+The names should match folders in the unzipped AWS folder in `/dist/awscli/boto3/data`
+
+Example:
+```
+s3
+sqs
+sns
+iam
+```
+
+4. Run the minify script passing the path to the unzipped aws dir.
+
+```
+$ python aws-cli-minify.py ./aws
+```
+
+## What the script does
+
+### Removes unused services
+Commands that arn't required are removed from awscli/boto3/data. Each command has its own folder with a number of json files. 
+While most of these file are fairly small the large number of services all add up.
+We also remove references to these services from endpoints.json.
+
+### Remove examples for unused services
+Examples relating to removed services are also removed saving some more space.
+
+### Minify all json files
+All whitespace and indentations are remove from json files.
+
+### Strips binaries
+As long as the `strip` command is availabe (typically installed from the bintools package in linux) all of the binary files are stripped of debug symbols.
+
+### Removing non-essential files
+
+| File              | Description                                            |
+| ----------------- | ------------------------------------------------------ |
+| `aws_completer`   | for scripts etc autocomplete isn't needed              |
+| `ac.index`        | a sqlite database of commands, CLI works without it.   |
+| `libsqlite3.so.0` | sqlite driver, not required since we deleted the db.   |
+
+## Size Reduction
+
+**Default AWS CLI**
+Zipped  - 64M
+Unzipped - 237M
+
+**Minified AWS CLI** 
+
+With only sqs, sns, s3, iam, rds & ecs enabled
+
+`zip -9 -r aws.min.zip aws`
+
+Zipped - 20M 
+Unzipped - 48M
+
+## TODO
+- Look at removing other binaries if present in the base image
+- Look at while some services (config, deploy opsworkscm) are required
+- Add option for automatically unzipping/rezipping the archive
---
+++ aws-cli-minify.py
@@ -0,0 +1,153 @@
+import argparse
+import json
+import os
+import shutil
+import subprocess
+import sys
+
+# TODO: Make this more configurable.
+#       Maybe check if the libs exist elsewhere in the os.
+files_to_remove = [
+    "aws_completer",
+    "libsqlite3.so.0",
+    "awscli/data/ac.index",
+]
+
+# TODO: CLI doesn't start when these services are removed.
+#       Possibly related to customizations/aliases
+required_services = [
+    'codedeploy',
+    'opsworkscm',
+    'config'
+]
+
+
+def check_base_dir_is_ok(base_dir):
+    base_dir_exists = os.path.isdir(base_dir)
+    aws_cli_exists = os.path.isfile(os.path.join(base_dir, 'aws'))
+    return base_dir_exists and aws_cli_exists
+
+def can_strip_files():
+    try:
+        subprocess.run(['strip', '--version'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
+        return True
+    except (subprocess.CalledProcessError, FileNotFoundError):
+        return False
+
+def _strip_files_in_directory(directory):
+    for filename in os.listdir(directory):
+        filepath = os.path.join(directory, filename)
+        if os.path.isfile(filepath):
+            print(f"stripping {filepath}")
+            subprocess.run(['strip', '-s', filepath])
+
+def strip_files(base_dir):
+    _strip_files_in_directory(base_dir)
+    _strip_files_in_directory(os.path.join(base_dir, "lib-dynload"))
+    _strip_files_in_directory(os.path.join(base_dir, "cryptography/hazmat/bindings"))
+
+def remove_examples(base_dir, services_to_keep):
+    directory = os.path.join(base_dir, "awscli/examples")
+    examples_removed=0
+    for item in os.listdir(directory):
+        item_path = os.path.join(directory, item)
+        if os.path.isdir(item_path) and os.path.basename(item) not in services_to_keep:
+            shutil.rmtree(item_path)
+            examples_removed+=1
+    print(f"removed {examples_removed} examples")
+
+def remove_services(base_dir, services_to_keep):
+    for s in services_to_keep:
+        print(f"keeping {s}")
+
+    directory = os.path.join(base_dir, "awscli/botocore/data")
+    services_removed=0
+    for item in os.listdir(directory):
+        item_path = os.path.join(directory, item)
+        if os.path.isdir(item_path) and os.path.basename(item) not in services_to_keep:
+            shutil.rmtree(item_path)
+            services_removed+=1
+    print(f"removed {services_removed} services")
+
+def clean_endpoints(base_dir, services_to_keep):
+    path_to_endpoints = os.path.join(base_dir, "awscli/botocore/data/endpoints.json")
+    endpoints = {}
+    endpoints_cleaned=0
+    with open(path_to_endpoints, 'r') as file:
+        endpoints = json.load(file)
+        for partition in endpoints['partitions']:
+            for service in list(partition['services'].keys()):
+                if service not in services_to_keep:
+                    del partition['services'][service]
+
+    with open(path_to_endpoints, 'w') as file:
+        print(json.dump(endpoints, file, separators=(',', ':')))
+        endpoints_cleaned+=1
+    print(f"removed {endpoints_cleaned} endpoints")
+
+def delete_files(base_dir, file_to_remove):
+    files_removed=0
+    for file_path in file_to_remove:
+        full_path = os.path.join(base_dir, file_path)
+        if os.path.isfile(full_path):
+            os.remove(full_path)
+            files_removed+=1
+    print(f"removed {files_removed} files")
+
+def minify_json(base_dir):
+    files_minified=0
+    for dirpath, _, filenames in os.walk(base_dir):
+        for filename in filenames:
+            if filename.endswith('.json'):
+                file_path = os.path.join(dirpath, filename)
+                with open(file_path, 'r') as file:
+                    content = json.load(file)
+
+                with open(file_path, 'w') as file:
+                    json.dump(content, file, separators=(',', ':'))
+                    files_minified+=1
+    print(f"minified {files_minified} json files")
+
+# A list of service names (as they appear in awscli/boto3/data)
+# separated by newlines.
+def load_services_to_keep(file_path):
+    with open(file_path, 'r') as file:
+        return [line.rstrip('\n') for line in file.readlines()]
+
+def minify(base_dir, to_keep):
+    remove_examples(base_dir, to_keep)
+    remove_services(base_dir, to_keep)
+    clean_endpoints(base_dir, to_keep)
+    minify_json(base_dir)
+    delete_files(base_dir, files_to_remove)
+    strip_files(base_dir)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Minify the aws cli.')
+    parser.add_argument('base_dir', type=str, help='Where the aws-cli has been unziped to.')
+    parser.add_argument('--services_to_keep', type=str, default=None, help='Comma-separated list of services to keep (optional).')
+    parser.add_argument('--filename', type=str, default=None, help='The filename to process (optional).')
+
+    args = parser.parse_args()
+
+    to_keep = []
+    if args.services_to_keep:
+        to_keep = args.services_to_keep.split(',')
+    else:
+        print("loading services to keep from minify.keep")
+        to_keep = load_services_to_keep("minify.keep")
+
+    if len(to_keep) == 0:
+        print("provide a minify.keep file or --services_to_keep param")
+        print("otherwise the cli will have no commands")
+        sys.exit(1)
+    to_keep = to_keep + required_services
+
+    base_dir = os.path.join(args.base_dir, 'dist')
+
+    if not check_base_dir_is_ok(base_dir):
+        print(f"invalid base directory: {base_dir}, expected dist/aws to exist")
+        sys.exit(1)
+
+    minify(base_dir, to_keep)
+
